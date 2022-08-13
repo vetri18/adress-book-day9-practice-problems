ef82913ad96fcf128aac812f92d84c21e3fa7099
vetrivel <vetrivel.kce@gmail.com>
2022-08-13 19:38:16 +0530

    UC1

---
+++ Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contact
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public int phone;
+        public string city;
+        public string state;
+        public int zipcode;
+        public string email;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace EmployeeWage
+﻿namespace AddressBook
 {
     class Program
     {
+        //creating list of contact
+        public static List<Contact> contactList = new List<Contact>();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Welcome to Address Book Sytem.");
+
+                Contact contact1 = new Contact();
+                Console.WriteLine("Enter first name: ");
+                contact1.firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter last name: ");
+                contact1.lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter address: ");
+                contact1.address = Console.ReadLine();
+
+                Console.WriteLine("Enter city: ");
+                contact1.city = Console.ReadLine();
+
+                Console.WriteLine("Enter state: ");
+                contact1.state = Console.ReadLine();
+
+                Console.WriteLine("Enter phone: ");
+                contact1.phone = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter email: ");
+                contact1.email = Console.ReadLine();
+
+                Console.WriteLine("Enter zipcode: ");
+                contact1.zipcode = Convert.ToInt32(Console.ReadLine());
+
+
+                contactList.Add(contact1);
         }
     }
 }
