1b25b671d062828dd6d08f158f4ddb193a57d7f5
vetrivel <vetrivel.kce@gmail.com>
2022-08-13 19:47:01 +0530

    edit contact UC3

--- Program.cs
+++ Program.cs
@@ -47,11 +47,68 @@
             }
             Console.WriteLine("Total number of contact in address book:" + contactList.Count);
 
+            //print contacts
+            Console.WriteLine("Current contacts in adress book:");
+
             foreach (Contact contact in contactList)
             {
                 Console.WriteLine(contact.firstName);
             }
 
+            Console.WriteLine("Do you want to edit contact details then press 1 or pres 2 for continue: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num == 1)
+            {
+                Console.WriteLine("Enter first name to edit details: ");
+                string firstName = Console.ReadLine();
+                bool found = false;
+                for (int i = 0; i < contactList.Count; i++)
+                {
+
+                    if (contactList[i].firstName == firstName)
+                    {
+                        found = true;  //found the contact
+
+                        //now editing...
+                        Console.WriteLine("Enter first name: ");
+                        contactList[i].firstName = Console.ReadLine();
+
+                        Console.WriteLine("Enter last name: ");
+                        contactList[i].lastName = Console.ReadLine();
+
+                        Console.WriteLine("Enter address: ");
+                        contactList[i].address = Console.ReadLine();
+
+                        Console.WriteLine("Enter city: ");
+                        contactList[i].city = Console.ReadLine();
+
+                        Console.WriteLine("Enter state: ");
+                        contactList[i].state = Console.ReadLine();
+
+                        Console.WriteLine("Enter phone: ");
+                        contactList[i].phone = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Enter email: ");
+                        contactList[i].email = Console.ReadLine();
+
+                        Console.WriteLine("Enter zipcode: ");
+                        contactList[i].zipcode = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    }
+                } //end of for loop
+                if (!found)
+                    Console.WriteLine("the contact with given person {0} is not in address book", firstName);
+                //print contacts
+                Console.WriteLine("Current contacts in adress book");
+                foreach (Contact contact in contactList)
+                {
+                    Console.WriteLine(contact.firstName);
+                }
+                Console.WriteLine("Do you want to edit contact press 1 to edit or press 2 to cancle.");
+                num = Convert.ToInt32(Console.ReadLine());
+            }//while loop end
         }
     }
 }
